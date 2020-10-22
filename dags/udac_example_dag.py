2673ddb3573d58f77e0e199d02107c5621e99648
Jeff <jeffdeagostino@gmail.com>
2020-10-22 15:11:17 -0400

    adding StageToRedshiftOperator logic

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -24,17 +24,30 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="log_data/{{execution-date.strftime('%Y')}}/{{execution-date.strftime('%m')}}/{{ds}}-events.json"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="song_data/{{execution-date.strftime('%Y')}}/{{execution-date.strftime('%m')}}/{{ds}}-events.json"
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials"
 )
 
 load_user_dimension_table = LoadDimensionOperator(
@@ -65,20 +78,20 @@ run_quality_checks = DataQualityOperator(
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
 # Configure Dependencies
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+#start_operator >> stage_songs_to_redshift
 
-stage_events_to_redshift >> load_songplays_table
-stage_songs_to_redshift >> load_songplays_table
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+#stage_songs_to_redshift >> load_songplays_table
 
-load_songplays_table >> load_user_dimension_table
-load_songplays_table >> load_song_dimension_table
-load_songplays_table >> load_artist_dimension_table
-load_songplays_table >> load_time_dimension_table
+load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table]
+#load_songplays_table >> load_song_dimension_table
+#oad_songplays_table >> load_artist_dimension_table
+#load_songplays_table >> load_time_dimension_table
 
-load_user_dimension_table >> run_quality_checks
-load_song_dimension_table >> run_quality_checks
-load_artist_dimension_table >> run_quality_checks
-load_time_dimension_table >> run_quality_checks
+[load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+#load_song_dimension_table >> run_quality_checks
+#load_artist_dimension_table >> run_quality_checks
+#load_time_dimension_table >> run_quality_checks
 
 run_quality_checks >> end_operator
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,9 +8,8 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
